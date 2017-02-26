6554e128e15de6ce648c7624b23b5067293d814e
Cynthia Lin <cynthialin1004@gmail.com>
2017-02-25 22:06:24 -0800

    issues.js: Add support for abandoning issues.

---
+++ src/issues/abandonIssue.js
@@ -0,0 +1,34 @@
+const GitHubApi = require("github"); // NodeJS wrapper for GitHub API
+const github = new GitHubApi(); // API client
+const cfg = require("../config.js"); // hidden config file
+const request = require("request");
+
+github.authenticate({ // Authentication
+  type: "basic",
+  username: cfg.username,
+  password: cfg.password
+});
+
+module.exports = exports = function(commenter, issueNumber, repoName, repoOwner) {
+  request({
+    uri: `https://api.github.com/repos/${repoOwner}/${repoName}/issues/${issueNumber}/assignees`,
+    method: "DELETE",
+    json: {
+      "assignees": [commenter]
+    },
+    headers: {
+      'User-Agent': 'zulipbot'
+    },
+    'auth': {
+      'username': cfg.username,
+      'password': cfg.password
+    }
+  }).on('response', (response) => {
+    github.issues.removeLabel({
+      owner: repoOwner,
+      repo: repoName,
+      number: issueNumber,
+      name: "in progress"
+    }).catch(console.error);
+  });
+}
