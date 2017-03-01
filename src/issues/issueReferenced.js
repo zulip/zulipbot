b5133da578fa8c6d4243be34bb569177ee521ab0
Joshua Pan <joshuapan8@gmail.com>
2017-03-01 00:30:31 -0800

    issueReferenced.js: Add "use strict".

--- src/issues/issueReferenced.js
+++ src/issues/issueReferenced.js
@@ -1,3 +1,5 @@
+"use strict"; // catch errors easier
+
 const GitHubApi = require("github"); // NodeJS wrapper for GitHub API
 const github = new GitHubApi(); // API client
 const cfg = require("../config.js"); // hidden config file
