42015429001aba97050e26c4c19d2a0e102d2b66
Cynthia Lin <cynthialin1004@gmail.com>
2017-03-05 20:45:20 -0800

    comments: Replace instances of "issue" with "pull request" on PRs.
    Fixes #47

--- src/issues/newComment.js
+++ src/issues/newComment.js
@@ -3,11 +3,26 @@
 const github = require("../github.js"); // GitHub wrapper initialization
 
 module.exports = exports = function(repoOwner, repoName, issueNumber, body) {
-  github.issues.createComment({
+  github.pullRequests.get({
     owner: repoOwner,
     repo: repoName,
-    number: issueNumber,
-    body: body
-  })
-  .catch(console.error);
+    number: issueNumber
+  }).then(() => {
+    body = body.replace(/issue/g, "pull request");
+    github.issues.createComment({
+      owner: repoOwner,
+      repo: repoName,
+      number: issueNumber,
+      body: body
+    })
+    .catch(console.error);
+  }, () => {
+    github.issues.createComment({
+      owner: repoOwner,
+      repo: repoName,
+      number: issueNumber,
+      body: body
+    })
+    .catch(console.error);
+  });
 };
