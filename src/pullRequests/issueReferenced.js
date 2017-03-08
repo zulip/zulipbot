8e92c8e693149fef7e136ab68405be06d52827ce
Joshua Pan <joshuapan8@gmail.com>
2017-03-08 01:50:27 -0800

    Prevent duplicate label team mentions. Fixes #50.

--- src/pullRequests/issueReferenced.js
+++ src/pullRequests/issueReferenced.js
@@ -8,27 +8,42 @@ module.exports = exports = function(body, pullRequestNumber, repoName, repoOwner
   const referencedIssueNumber = body.match(/#([0-9]+)/)[1];
   let issueLabels = []; // initialize array for area labels
   let labelTeams = [];
-  github.issues.getIssueLabels({ // get referenced issue labels
+  let mentionedLabels = [];
+  github.issues.getComments({ // get comments of issue
     owner: repoOwner,
     repo: repoName,
-    number: referencedIssueNumber
-  }).then((issueLabelArray) => {
-    issueLabelArray.data.forEach((issueLabel) => {
-      const labelName = issueLabel.name; // label name
-      if (areaLabels.has(labelName) && !issueLabels.includes(labelName)) {
-        issueLabels.push(labelName); // push all associated area labels to array
-        labelTeams.push(areaLabels.get(labelName)); // push all associated area labels to array
-      }
-    }); // add all issue label names and area label teams to issueLabels to labelTeams
-    const areaLabelTeams = labelTeams.join(`, @${repoOwner}/`);
-    const referencedAreaLabels = issueLabels.join("\", \""); // join corresponding area label teams into one string
-    let labelGrammar;
-    if (labelTeams.length > 1) {
-      labelGrammar = "labels";
-    } else if (labelTeams.length === 1) {
-      labelGrammar = "label";
-    } else return;
-    const comment = `Hello @${repoOwner}/${areaLabelTeams} members, this pull request references an issue with the "${referencedAreaLabels}" ${labelGrammar}, so you may want to check it out!`; // comment template
-    newComment(repoOwner, repoName, pullRequestNumber, comment); // create comment
+    number: pullRequestNumber,
+    per_page: 100
+  }).then((pullComments) => {
+    if (pullComments.data.length) {
+      pullComments.data.forEach((pullComment) => {
+        if (pullComment.body.includes("this pull request references") && pullComment.user.login === "zulipbot") {
+          mentionedLabels = mentionedLabels.concat(pullComment.body.match(/"(.*?)"/g));
+        }
+      });
+    }
+    github.issues.getIssueLabels({ // get referenced issue labels
+      owner: repoOwner,
+      repo: repoName,
+      number: referencedIssueNumber
+    }).then((issueLabelArray) => {
+      issueLabelArray.data.forEach((issueLabel) => {
+        const labelName = issueLabel.name; // label name
+        if (areaLabels.has(labelName) && !issueLabels.includes(labelName) && !mentionedLabels.includes("\"" + labelName + "\"")) { // make sure the label team hasn't been mentioned yet
+          issueLabels.push(labelName); // push all associated area labels to array
+          labelTeams.push(areaLabels.get(labelName)); // push all associated area labels to array
+        }
+      }); // add all issue label names and area label teams to issueLabels to labelTeams
+      const areaLabelTeams = labelTeams.join(`, @${repoOwner}/`);
+      const referencedAreaLabels = issueLabels.join("\", \""); // join corresponding area label teams into one string
+      let labelGrammar;
+      if (labelTeams.length > 1) {
+        labelGrammar = "labels";
+      } else if (labelTeams.length === 1) {
+        labelGrammar = "label";
+      } else return;
+      const comment = `Hello @${repoOwner}/${areaLabelTeams} members, this pull request references an issue with the "${referencedAreaLabels}" ${labelGrammar}, so you may want to check it out!`; // comment template
+      newComment(repoOwner, repoName, pullRequestNumber, comment); // create comment
+    });
   });
 };
