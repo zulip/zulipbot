bd569588e8528dfac884deda76a7c41c510b6cb1
Joshua Pan <joshuapan8@gmail.com>
2017-03-08 18:31:13 -0800

    Fix issue/pull request comment bug. Fixes #53.

--- src/issues/newComment.js
+++ src/issues/newComment.js
@@ -3,27 +3,11 @@
 const github = require("../github.js"); // GitHub wrapper initialization
 
 module.exports = exports = function(repoOwner, repoName, issueNumber, body) {
-  github.pullRequests.get({
+  github.issues.createComment({
     owner: repoOwner,
     repo: repoName,
-    number: issueNumber
-  }).then((response) => {
-    body = body.replace(/issue/g, "pull request");
-    if (response.data.title.includes("WIP") && body.includes("@") && !body.includes("Error")) return;
-    github.issues.createComment({
-      owner: repoOwner,
-      repo: repoName,
-      number: issueNumber,
-      body: body
-    })
-    .catch(console.error);
-  }, () => {
-    github.issues.createComment({
-      owner: repoOwner,
-      repo: repoName,
-      number: issueNumber,
-      body: body
-    })
-    .catch(console.error);
-  });
+    number: issueNumber,
+    body: body
+  })
+  .catch(console.error);
 };
