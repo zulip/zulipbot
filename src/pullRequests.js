45648323868a9c1391e332142508c786d69102e7
Cynthia Lin <cynthialin1004@gmail.com>
2017-03-14 19:35:31 -0700

    pull requests: Check opened/edited PRs for commit-issue references.
    Fixes #60

--- src/pullRequests.js
+++ src/pullRequests.js
@@ -3,27 +3,27 @@
 const github = require("./github.js"); // GitHub wrapper initialization
 const issueAreaLabeled = require("./issues/issueAreaLabeled.js"); // issue labeled with area label
 const issueReferenced = require("./pullRequests/issueReferenced.js"); // create comment
+const commitReference = require("./pullRequests/commitReference.js"); // create comment
 
 module.exports = exports = function(payload) {
   // get necessary information from request body
   const action = payload.action;
   let body, addedLabel; // initialize variables for pull request review (comment) body and added label
+  const pullRequestNumber = payload.pull_request.number; // number of PR
+  const repoName = payload.repository.name; // PR repository
+  const repoOwner = payload.repository.owner.login; // repository owner
   if (action === "opened") { // if pull request was opened
     body = payload.pull_request.body; // contents of PR body
+    commitReference(body, pullRequestNumber, repoName, repoOwner);
   } else if (action === "submitted") { // if pull request review was submitted
     body = payload.review.body; // contents of PR review
   } else if (action === "created") { // if PR review comment was created
     body = payload.comment.body; // contents of PR review comment
+  } else if (action === "synchronize") { // when PR is synchronized (commits modified)
+    commitReference(body, pullRequestNumber, repoName, repoOwner); // check if edited commits reference an issue
+    return;
   } else if (action === "labeled") {
     addedLabel = payload.label.name;
-  } else return;
-  const pullRequestNumber = payload.pull_request.number; // number of PR
-  const repoName = payload.repository.name; // PR repository
-  const repoOwner = payload.repository.owner.login; // repository owner
-  if (body && body.match(/#([0-9]+)/)) {
-    issueReferenced(body, pullRequestNumber, repoName, repoOwner);
-  }
-  if (addedLabel) {
     github.issues.getIssueLabels({
       owner: repoOwner,
       repo: repoName,
@@ -31,6 +31,8 @@ module.exports = exports = function(payload) {
     }).then((response) => {
       const issueLabelArray = response.data;
       issueAreaLabeled(addedLabel, pullRequestNumber, repoName, repoOwner, issueLabelArray);
+      return;
     });
-  }
+  } else return;
+  if (body.match(/#([0-9]+)/)) issueReferenced(body, pullRequestNumber, repoName, repoOwner);
 };
