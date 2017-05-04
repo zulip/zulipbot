02b9e05e7809564f859c59635b2a278b8d27d1b4
Cynthia Lin <cynthialin1004@gmail.com>
2017-05-04 08:26:02 -0700

    config: Add checkMergeConflicts option.

--- src/zulip_project_config.js
+++ src/zulip_project_config.js
@@ -11,6 +11,7 @@ module.exports = {
   selfLabelingOnly: true, // enable/disable only issue/PR author can label their issue/PR
   joinCommands: [], // configured aliases for "join" command
   commitReferenceEnabled: true, // enable/disable commit reference notifications for label teams
+  checkMergeConflicts: true, // enable/disable merge conflict warnings on PRs
   escapeWIPString: "WIP", // disable mentioning of teams on PRs with WIP string in title; teams will always be notified if null
   areaLabels: new Map([ // map of area labels and corresponding teams; area label team references will not be enabled if null
     ["area: analytics", "server-analytics"], // format: ["label name", "team-slug"]
