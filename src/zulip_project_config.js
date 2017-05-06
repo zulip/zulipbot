638d717186c4081b63806a0f6cd9ad26ce9522c2
Cynthia Lin <cynthialin1004@gmail.com>
2017-05-06 10:36:23 -0700

    tools: Add fetchPriority tool.
    Fetches open unclaimed issues with configured priority label.

--- src/zulip_project_config.js
+++ src/zulip_project_config.js
@@ -63,5 +63,6 @@ module.exports = {
   inactiveLabel: "inactive", // label for inactive issues/PRs that zulipbot will not track for activity; will track all issues/PRs for activity if null
   reviewedLabel: "reviewed", // label for reviewed PRs; review system disabled if null
   needsReviewLabel: "needs review", // label for PRs needign review; review system disabled if null
+  priorityLabel: "priority", // label for unclaimed "priority" issues daily digest
   pullRequestsAssignee: false // enable/disable assigning PR reviewers to PRs
 };
