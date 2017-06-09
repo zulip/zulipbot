895b8314c8d460aa074962e987121b1091acb695
Joshua Pan <joshuapan8@gmail.com>
2017-06-08 21:40:47 -0700

    config: Add i18n area team.

--- src/zulip_project_config.js
+++ src/zulip_project_config.js
@@ -30,6 +30,7 @@ module.exports = {
     ["area: emoji", "server-emoji"],
     ["area: export", "server-misc"],
     ["area: hotkeys", "server-hotkeys"],
+    ["area: i18n", "server-i18n"],
     ["area: in", "server-in"],
     ["area: integrations", "server-integrations"],
     ["area: left-sidebar", "server-sidebars"],
