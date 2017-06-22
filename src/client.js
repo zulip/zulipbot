c7bd3e651b20adf65c6dd5fbb8fa4951597538b0
Cynthia Lin <cynthialin1004@gmail.com>
2017-06-22 11:02:31 -0700

    refactoring: Bind newComment method to `client`.

--- src/client.js
+++ src/client.js
@@ -3,11 +3,20 @@ const client = new GitHub();
 client.cfg = require("./config.js");
 client.zulip = require("zulip-js")(client.cfg.zulip);
 
-client.authenticate({ // Authentication
+client.authenticate({
   type: "basic",
   username: client.cfg.username,
   password: client.cfg.password
 });
+
+client.newComment = (issue, repository, body, replacePR) => {
+  const issueNumber = issue.number;
+  const repoName = repository.name;
+  const repoOwner = repository.owner.login;
+  if (replacePR) body = body.replace("[payload]", "pull request");
+  else body = body.replace("[payload]", "issue");
+  client.issues.createComment({owner: repoOwner, repo: repoName, number: issueNumber, body: body});
+};
 
 client.send = (msg, topic) => {
   const params = {
