46bb91ef55c9c97ba74f558509ad1d23665a309e
Cynthia Lin <cynthialin1004@gmail.com>
2017-06-29 10:27:38 -0700

    pull requests: Check issue references only in PR bodies and commits.

--- src/pullRequests.js
+++ src/pullRequests.js
@@ -1,12 +1,7 @@
-"use strict"; // catch errors easier
+const commitReference = require("./pullRequests/commitReference.js");
 
-const issueReferenced = require("./pullRequests/issueReferenced.js"); // pull request references an issue
-const commitReference = require("./pullRequests/commitReference.js"); // create comment
-
-module.exports = exports = (payload, client) => {
-  // get necessary information from request body
+exports.run = (client, payload) => {
   const action = payload.action;
-  let body; // initialize variables for pull request review (comment) body and added label
   const pullRequestNumber = payload.pull_request.number; // number of PR
   const repoName = payload.repository.name; // PR repository
   const repoOwner = payload.repository.owner.login; // repository owner
@@ -18,11 +13,9 @@ module.exports = exports = (payload, client) => {
   }).then((response) => {
     response.data.forEach(label => labels.push(label.name));
     if (action === "opened") { // if pull request was opened
-      body = payload.pull_request.body; // contents of PR body
-      if (client.cfg.areaLabels && client.cfg.commitReferenceEnabled) commitReference(client, body, pullRequestNumber, repoName, repoOwner);
+      if (client.cfg.areaLabels && client.cfg.commitReferenceEnabled) commitReference(client, payload.pull_request.body, pullRequestNumber, repoName, repoOwner);
       if (client.cfg.reviewedLabel && client.cfg.needsReviewLabel) client.issues.addLabels({owner: repoOwner, repo: repoName, number: pullRequestNumber, labels: [client.cfg.needsReviewLabel]}).catch(console.error); // add labels
     } else if (action === "submitted") { // if pull request review was submitted
-      body = payload.review.body; // contents of PR review
       const reviewer = payload.review.user.login; // reviewer username
       const author = payload.pull_request.user.login; // PR opener
       if (client.cfg.reviewedLabel && client.cfg.needsReviewLabel && labels.indexOf(client.cfg.needsReviewLabel) !== -1 && reviewer !== author) {
@@ -30,15 +23,12 @@ module.exports = exports = (payload, client) => {
         replaceLabels(client, repoOwner, repoName, pullRequestNumber, labels);
         if (client.cfg.pullRequestsAssignee) client.issues.addAssigneesToIssue({owner: repoOwner, repo: repoName, number: pullRequestNumber, assignees: [reviewer]}).catch(console.error);
       }
-    } else if (action === "created") { // if PR review comment was created
-      body = payload.comment.body; // contents of PR review comment
     } else if (action === "synchronize") { // when PR is synchronized (commits modified)
-      if (client.cfg.areaLabels && client.cfg.commitReferenceEnabled) commitReference(client, body, pullRequestNumber, repoName, repoOwner); // check if edited commits reference an issue
+      if (client.cfg.areaLabels && client.cfg.commitReferenceEnabled) commitReference(client, payload.pull_request.body, pullRequestNumber, repoName, repoOwner); // check if edited commits reference an issue
       if (client.cfg.reviewedLabel && client.cfg.needsReviewLabel && labels.indexOf(client.cfg.reviewedLabel) !== -1) {
         labels[labels.indexOf(client.cfg.reviewedLabel)] = client.cfg.needsReviewLabel;
         replaceLabels(client, repoOwner, repoName, pullRequestNumber, labels);
       }
-      return;
     } else if (action === "labeled" && client.cfg.areaLabels) {
       client.issues.get({owner: repoOwner, repo: repoName, number: pullRequestNumber})
       .then((response) => {
@@ -58,8 +48,7 @@ module.exports = exports = (payload, client) => {
         labels.splice(labels.indexOf(needsReviewLabel), 1);
       } else return;
       replaceLabels(client, repoOwner, repoName, pullRequestNumber, labels);
-    } else return;
-    if (body && body.match(/#([0-9]+)/) && action !== "opened" && client.cfg.areaLabels) issueReferenced(client, body, pullRequestNumber, repoName, repoOwner);
+    }
   });
 };
 
