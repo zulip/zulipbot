15befa3a0a35109694cc826547bec79f1a590d2b
Cynthia Lin <cynthialin1004@gmail.com>
2017-07-26 15:53:23 -0700

    testing: Remove coverage testing and update Eslint rules.
    `gulp-istanbul` is incompatible with ES6 syntax introduced in previous commit.
    
    Since no coverage tests have been written thus far, a framework can always be readded later.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 "use strict";
-var path = require("path");
-var gulp = require("gulp");
-var eslint = require("gulp-eslint");
-var excludeGitignore = require("gulp-exclude-gitignore");
-var mocha = require("gulp-mocha");
-var istanbul = require("gulp-istanbul");
-var nsp = require("gulp-nsp");
-var plumber = require("gulp-plumber");
-var coveralls = require("gulp-coveralls");
+const path = require("path");
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const excludeGitignore = require("gulp-exclude-gitignore");
+const nsp = require("gulp-nsp");
 
 gulp.task("static", () => {
   return gulp.src(["**/*.js", "tools/*", "!tools/setup"])
@@ -21,40 +17,5 @@ gulp.task("nsp", (cb) => {
   nsp({package: path.resolve("package.json")}, cb);
 });
 
-gulp.task("pre-test", () => {
-  return gulp.src("src/**/*.js")
-  .pipe(excludeGitignore())
-  .pipe(istanbul({
-    includeUntested: true
-  }))
-  .pipe(istanbul.hookRequire());
-});
-
-gulp.task("test", ["pre-test"], (cb) => {
-  let mochaErr;
-
-  gulp.src("test/**/*.js")
-  .pipe(plumber())
-  .pipe(mocha({reporter: "spec"}))
-  .on("error", (err) => {
-    mochaErr = err;
-  })
-  .pipe(istanbul.writeReports())
-  .on("end", () => {
-    cb(mochaErr);
-  });
-});
-
-gulp.task("watch", () => {
-  gulp.watch(["src/**/*.js", "test/**"], ["test"]);
-});
-
-gulp.task("coveralls", ["test"], () => {
-  if (!process.env.CI) return;
-
-  return gulp.src(path.join(__dirname, "coverage/lcov.info"))
-  .pipe(coveralls());
-});
-
 gulp.task("prepublish", ["nsp"]);
-gulp.task("default", ["static", "test", "coveralls"]); // ["static", "test", "coveralls"]
+gulp.task("default", ["static"]); // ["static", "test", "coveralls"]
