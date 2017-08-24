da672944522165a0fe44a3fea68ad4ac0e8a0c60
Cynthia Lin <cynthialin1004@gmail.com>
2017-08-24 10:58:57 -0700

    pull requests: Refactor merge conflicts to check for inactive label.
    Fixes #115

--- src/automations/checkMergeConflicts.js
+++ src/automations/checkMergeConflicts.js
@@ -20,27 +20,28 @@ exports.check = async function(client, number, repoName, repoOwner) {
   const author = pull.data.user.login;
   const comment = client.templates.get("mergeConflictWarning").replace("[username]", author)
   .replace("[repoOwner]", repoOwner).replace("[repoName]", repoName);
-  const commits = await client.pullRequests.getCommits({
-    owner: repoOwner, repo: repoName, number: number, per_page: 100
-  });
-  const lastCommitTime = Date.parse(commits.data.slice(-1).pop().commit.committer.date);
   const comments = await client.issues.getComments({
     owner: repoOwner, repo: repoName, number: number, per_page: 100
   });
-  const labelComment = comments.data.find((c) => {
-    const synchCheck = lastCommitTime < Date.parse(c.updated_at);
-    return c.body.includes(comment) && synchCheck && c.user.login === client.cfg.username;
+  const warnings = comments.data.filter((c) => {
+    return c.body.includes(comment) && c.user.login === client.cfg.username;
   });
-  if (!labelComment && mergeable === false) {
-    client.newComment(pull.data, pull.data.base.repo, comment);
-  } else if (mergeable) {
-    const oldComments = comments.data.filter((c) => {
-      return c.body.includes(comment) && c.user.login === client.cfg.username;
-    }).map(c => c.id);
-    oldComments.forEach((c) => {
+  if (mergeable && warnings.length) {
+    warnings.forEach((c) => {
       client.issues.deleteComment({
-        owner: repoOwner, repo: repoName, id: c
+        owner: repoOwner, repo: repoName, id: c.id
       });
     });
+  } else if (mergeable === false) {
+    const commits = await client.pullRequests.getCommits({
+      owner: repoOwner, repo: repoName, number: number, per_page: 100
+    });
+    const lastCommitTime = Date.parse(commits.data.slice(-1).pop().commit.committer.date);
+    const labelComment = warnings.find(c => lastCommitTime < Date.parse(c.updated_at));
+    const labels = await client.issues.get({
+      owner: repoOwner, repo: repoName, number: number
+    });
+    const inactive = labels.data.find(l => l.name === client.cfg.inactiveLabel);
+    if (!labelComment && !inactive) client.newComment(pull.data, pull.data.base.repo, comment);
   }
 };
