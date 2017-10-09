390f60b43de69120d8fa64992de2d42a8317adef
Cynthia Lin <cynthialin1004@gmail.com>
2017-10-08 22:17:14 -0700

    refactoring: Refactor PR inactivity checks to use array iteration.

--- src/automations/checkInactivity.js
+++ src/automations/checkInactivity.js
@@ -4,7 +4,7 @@ exports.run = (client) => {
       const repoOwner = repo.split("/")[0];
       const repoName = repo.split("/")[1];
       const func = client.pullRequests.getAll({
-        owner: repoOwner, repo: repoName, per_page: 100, sort: "long-running", direction: "desc"
+        owner: repoOwner, repo: repoName, per_page: 100
       });
       return all.concat(client.getAll(client, [], func));
     }, [])
@@ -12,58 +12,56 @@ exports.run = (client) => {
     const pullRequests = array.reduce((a, element) => {
       return a.concat(element);
     }, []);
-    await scrapeInactivePullRequests(client, pullRequests);
+    await scrapePullRequests(client, pullRequests);
   });
 };
 
-async function scrapeInactivePullRequests(client, pullRequests) {
+async function scrapePullRequests(client, pullRequests) {
   const references = new Map();
   const ims = client.cfg.inactivityTimeLimit * 86400000;
-  pullRequests.forEach(async(pullRequest, index, array) => {
-    setTimeout(async() => {
-      const time = Date.parse(pullRequest.updated_at);
-      const body = pullRequest.body;
-      const number = pullRequest.number;
-      const author = pullRequest.user.login;
-      const repoName = pullRequest.base.repo.name;
-      const repoOwner = pullRequest.base.repo.owner.login;
-      if (time + ims <= Date.now()) {
-        const labels = await client.issues.getIssueLabels({
-          owner: repoOwner, repo: repoName, number: number
-        });
-        const inactiveLabel = labels.data.find(l => l.name === client.cfg.inactiveLabel);
-        const reviewedLabel = labels.data.find(l => l.name === client.cfg.reviewedLabel);
-        if (inactiveLabel || !reviewedLabel) return;
-        const comment = client.templates.get("updateWarning").replace("[author]", author)
-        .replace("[days]", client.cfg.inactivityTimeLimit);
-        const comments = await client.issues.getComments({
-          owner: repoOwner, repo: repoName, number: number, per_page: 100
-        });
-        const com = comments.data.slice(-1).pop();
-        const lastComment = com ? com.body.includes(comment) && com.user.login === client.cfg.username : false;
-        if (reviewedLabel && !lastComment) {
-          client.newComment(pullRequest, pullRequest.base.repo, comment);
-        }
-      }
-      const commits = await client.pullRequests.getCommits({
+  const iterator = pullRequests[Symbol.iterator]();
+  for (let pullRequest of iterator) {
+    const time = Date.parse(pullRequest.updated_at);
+    const body = pullRequest.body;
+    const number = pullRequest.number;
+    const author = pullRequest.user.login;
+    const repoName = pullRequest.base.repo.name;
+    const repoOwner = pullRequest.base.repo.owner.login;
+    if (time + ims <= Date.now()) {
+      const labels = await client.issues.getIssueLabels({
         owner: repoOwner, repo: repoName, number: number
       });
-      const refIssues = commits.data.filter((c) => {
-        return c.commit.message.match(/#([0-9]+)/);
-      }).map(c => c.commit.message);
-      if (body.match(/#([0-9]+)/) || refIssues.length) {
-        const commitRef = refIssues[0] ? refIssues[0].match(/#([0-9]+)/)[1] : null;
-        const ref = commitRef || body.match(/#([0-9]+)/)[1];
-        references.set(`${repoName}/${ref}`, time);
-      }
-      if (index !== array.length - 1) return;
-      const func = client.issues.getAll({
-        filter: "all", labels: client.cfg.inProgressLabel, per_page: 100
+      const inactiveLabel = labels.data.find(l => l.name === client.cfg.inactiveLabel);
+      const reviewedLabel = labels.data.find(l => l.name === client.cfg.reviewedLabel);
+      if (inactiveLabel || !reviewedLabel) return;
+      const comment = client.templates.get("updateWarning").replace("[author]", author)
+      .replace("[days]", client.cfg.inactivityTimeLimit);
+      const comments = await client.issues.getComments({
+        owner: repoOwner, repo: repoName, number: number, per_page: 100
       });
-      const issues = await client.getAll(client, [], func);
-      await scrapeInactiveIssues(client, references, issues);
-    }, index * 500);
+      const com = comments.data.slice(-1).pop();
+      const lastComment = com ? com.body.includes(comment) && com.user.login === client.cfg.username : false;
+      if (reviewedLabel && !lastComment) {
+        client.newComment(pullRequest, pullRequest.base.repo, comment);
+      }
+    }
+    const commits = await client.pullRequests.getCommits({
+      owner: repoOwner, repo: repoName, number: number
+    });
+    const refIssues = commits.data.filter((c) => {
+      return c.commit.message.match(/#([0-9]+)/);
+    }).map(c => c.commit.message);
+    if (body.match(/#([0-9]+)/) || refIssues.length) {
+      const commitRef = refIssues[0] ? refIssues[0].match(/#([0-9]+)/)[1] : null;
+      const ref = commitRef || body.match(/#([0-9]+)/)[1];
+      references.set(`${repoName}/${ref}`, time);
+    }
+  }
+  const func = client.issues.getAll({
+    filter: "all", labels: client.cfg.inProgressLabel, per_page: 100
   });
+  const issues = await client.getAll(client, [], func);
+  await scrapeInactiveIssues(client, references, issues);
 }
 
 async function scrapeInactiveIssues(client, references, issues) {
