4ea9c3cbd854c5a2be24819a9aa9def674e560c9
Cynthia Lin <cynthialin1004@gmail.com>
2018-03-29 16:43:54 -0700

    refactoring: Move templates folder to config folder.

--- src/client.js
+++ src/client.js
@@ -9,18 +9,21 @@ client.events = new Map();
 client.invites = new Map();
 client.templates = new Map();
 
+const automations = fs.readdirSync(`${__dirname}/automations`);
+for (const file of automations) {
+  let data = require(`./automations/${file}`);
+  for (let method of Object.keys(data)) {
+    data[method] = data[method].bind(client);
+  }
+  client.automations.set(file.slice(0, -3), data);
+}
+
 const commands = fs.readdirSync(`${__dirname}/commands`);
 for (const file of commands) {
   const data = require(`./commands/${file}`);
   for (let i = data.aliases.length; i--;) {
     client.commands.set(data.aliases[i], data);
   }
-}
-
-const templates = fs.readdirSync(`${__dirname}/templates`);
-for (const file of templates) {
-  const text = fs.readFileSync(`${__dirname}/templates/${file}`, "utf8");
-  client.templates.set(file.slice(0, -3), text);
 }
 
 const events = fs.readdirSync(`${__dirname}/events`);
@@ -31,13 +34,11 @@ for (const event of events) {
   }
 }
 
-const automations = fs.readdirSync(`${__dirname}/automations`);
-for (const file of automations) {
-  let data = require(`./automations/${file}`);
-  for (let method of Object.keys(data)) {
-    data[method] = data[method].bind(client);
-  }
-  client.automations.set(file.slice(0, -3), data);
+const configPath = `${__dirname}/../config`;
+const templates = fs.readdirSync(`${configPath}/templates`);
+for (const file of templates) {
+  const text = fs.readFileSync(`${configPath}/templates/${file}`, "utf8");
+  client.templates.set(file.slice(0, -3), text);
 }
 
 client.authenticate({
