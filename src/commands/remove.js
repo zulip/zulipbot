19f2d9e957d0c62c44b7a30aa9956b84e514eda6
Cynthia Lin <cynthialin1004@gmail.com>
2018-08-26 21:45:23 -0700

    tests: Add remove command test coverage.

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -1,9 +1,11 @@
+const respond = value => new Promise(resolve => resolve(value));
+
 exports.run = async function(payload, commenter, args) {
   const creator = payload.issue.user.login;
   const self = this.cfg.issues.commands.label.self;
   const selfLabel = self.users ? !self.users.includes(commenter) : self;
   const forbidden = selfLabel && creator !== commenter;
-  if (forbidden || !args.match(/".*?"/)) return;
+  if (forbidden || !args.match(/".*?"/)) return respond(false);
 
   const repoName = payload.repository.name;
   const repoOwner = payload.repository.owner.login;
@@ -18,7 +20,7 @@ exports.run = async function(payload, commenter, args) {
     owner: repoOwner, repo: repoName, number: number, labels: removeLabels
   });
 
-  if (!rejected.length) return;
+  if (!rejected.length) return respond(true);
 
   const one = rejected.length === 1;
   const type = payload.issue.pull_request ? "pull request" : "issue";
@@ -30,7 +32,7 @@ exports.run = async function(payload, commenter, args) {
     beState: `w${one ? "as" : "ere"}`, action: "removed from"
   });
 
-  this.issues.createComment({
+  return this.issues.createComment({
     owner: repoOwner, repo: repoName, number: number, body: error
   });
 };
---
+++ test/commands/remove.js
@@ -0,0 +1,152 @@
+const simple = require("simple-mock");
+const test = require("tap").test;
+
+const client = require(`${__dirname}/../../src/client.js`);
+const remove = require(`${__dirname}/../../src/commands/remove.js`);
+
+const payload = {
+  repository: {
+    owner: {
+      login: "zulip"
+    },
+    name: "zulipbot"
+  },
+  issue: {
+    pull_request: false,
+    number: 69,
+    labels: [
+      {name: "bug"},
+      {name: "help wanted"}
+    ],
+    user: {
+      login: "octocat"
+    }
+  }
+};
+
+test("Reject if self-labelling enabled with different commenter", async t => {
+  client.cfg.issues.commands.label.self = true;
+  const commenter = "octokitten";
+  const args = "\"bug\"";
+  const response = await remove.run.call(client, payload, commenter, args);
+  t.is(response, false);
+
+  simple.restore();
+  t.end();
+});
+
+test("Reject if self-labelling users excludes commenter", async t => {
+  client.cfg.issues.commands.label.self = {
+    users: ["octocat"]
+  };
+  const commenter = "octokitten";
+  const args = "\"bug\"";
+  const response = await remove.run.call(client, payload, commenter, args);
+  t.is(response, false);
+
+  simple.restore();
+  t.end();
+});
+
+test("Reject if invalid arguments were provided", async t => {
+  const commenter = "octocat";
+  const args = "no arguments";
+  const response = await remove.run.call(client, payload, commenter, args);
+  t.is(response, false);
+
+  simple.restore();
+  t.end();
+});
+
+test("Remove appropriate labels", async t => {
+  client.cfg.issues.commands.label.self = false;
+  const commenter = "octocat";
+  const args = "\"bug\"";
+
+  const request = simple.mock(client.issues, "replaceAllLabels")
+    .resolveWith({
+      code: 200
+    });
+
+  const response = await remove.run.call(client, payload, commenter, args);
+
+  t.equals(request.lastCall.arg.owner, "zulip");
+  t.equals(request.lastCall.arg.repo, "zulipbot");
+  t.equals(request.lastCall.arg.number, 69);
+  t.same(request.lastCall.arg.labels, ["help wanted"]);
+  t.equals(response, true);
+
+  simple.restore();
+  t.end();
+});
+
+test("Remove appropriate labels with single rejection message", async t => {
+  payload.issue.pull_request = false;
+  const commenter = "octocat";
+  const args = "\"help wanted\" \"enhancement\"";
+
+  const request = simple.mock(client.issues, "replaceAllLabels")
+    .resolveWith({
+      code: 200
+    });
+
+  const error = "**ERROR:** Label \"enhancement\" does not exist and was thus not removed from this issue.";
+
+  const request2 = simple.mock(client.issues, "createComment")
+    .resolveWith({
+      data: {
+        body: error
+      }
+    });
+
+  const response = await remove.run.call(client, payload, commenter, args);
+
+  t.equals(request.lastCall.arg.owner, "zulip");
+  t.equals(request.lastCall.arg.repo, "zulipbot");
+  t.equals(request.lastCall.arg.number, 69);
+  t.same(request.lastCall.arg.labels, ["bug"]);
+  t.equals(request2.lastCall.arg.owner, "zulip");
+  t.equals(request2.lastCall.arg.repo, "zulipbot");
+  t.equals(request2.lastCall.arg.number, 69);
+  t.equals(request2.lastCall.arg.body, error);
+  t.equals(response.data.body, error);
+
+  simple.restore();
+  t.end();
+});
+
+test("Remove appropriate labels with multiple rejection message", async t => {
+  payload.issue.pull_request = true;
+  client.cfg.issues.commands.label.self = false;
+  const commenter = "octocat";
+  const args = "\"help wanted\" \"enhancement\" \"duplicate\"";
+
+  const request = simple.mock(client.issues, "replaceAllLabels")
+    .resolveWith({
+      code: 200
+    });
+
+  const error = "**ERROR:** Labels \"enhancement\", \"duplicate\" do not exist and were thus not removed from this pull request.";
+
+  const request2 = simple.mock(client.issues, "createComment")
+    .resolveWith({
+      data: {
+        body: error
+      }
+    });
+
+  const response = await remove.run.call(client, payload, commenter, args);
+
+  t.equals(request.lastCall.arg.owner, "zulip");
+  t.equals(request.lastCall.arg.repo, "zulipbot");
+  t.equals(request.lastCall.arg.number, 69);
+  t.same(request.lastCall.arg.labels, ["bug"]);
+  t.equals(request2.lastCall.arg.owner, "zulip");
+  t.equals(request2.lastCall.arg.repo, "zulipbot");
+  t.equals(request2.lastCall.arg.number, 69);
+  t.equals(request2.lastCall.arg.body, error);
+  t.equals(response.data.body, error);
+
+  simple.restore();
+  t.end();
+});
