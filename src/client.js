3dde0ee7491957202087854d081d2c02b9c5a375
Anders Kaseorg <anders@zulip.com>
2021-09-15 14:18:28 -0700

    Add Octokit retry and throttling plugins.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import fs from "fs";
 
+import { retry } from "@octokit/plugin-retry";
+import { throttling } from "@octokit/plugin-throttling";
 import { Octokit } from "@octokit/rest";
 import _ from "lodash";
 
@@ -12,9 +14,36 @@ import * as responses from "./events/responses/index.js";
 import Template from "./structures/template.js";
 import * as util from "./util.js";
 
+const MyOctokit = Octokit.plugin(retry, throttling);
+
 const cfg = _.merge({}, defaults, custom);
-const client = new Octokit({
+const client = new MyOctokit({
   auth: cfg.auth.oAuthToken,
+  retry: {
+    enabled: process.env.NODE_ENV !== "test",
+  },
+  throttle: {
+    enabled: process.env.NODE_ENV !== "test",
+    onRateLimit: (retryAfter, { method, url, request: { retryCount } }) => {
+      if (retryCount < 3) {
+        client.log.warn(
+          `Rate limit exceeded ${
+            retryCount + 1
+          } times for ${method} ${url}; retrying in ${retryAfter} seconds`
+        );
+        return true;
+      }
+
+      client.log.warn(
+        `Rate limit exceeded ${
+          retryCount + 1
+        } times for ${method} ${url}; aborting`
+      );
+    },
+    onAbuseLimit: (retryAfter, { method, url }) => {
+      client.log.warn(`Abuse detected for ${method} ${url}; aborting`);
+    },
+  },
 });
 client.cfg = cfg;
 client.util = { ...util };
